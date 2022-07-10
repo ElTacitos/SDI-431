8284eaffa091d6c33d6873dd1f3fea04a425feb4
eltacitos <hugo.locatelli1@gmail.com>
2022-07-10 18:59:35 +0200

    changed the store-front ci-cd

--- sdi-app/components/Home.vue
+++ sdi-app/components/Home.vue
@@ -3,31 +3,10 @@
     <h1>SDI 431</h1>
     <h2>The place you go to fill you fish tanks, socks drawers and tractor garages</h2>
     <ProductList/>
-    <div class="footer">
-      <button @click="startStrapi">Start Strapi</button>
-      <button @click="stopStrapi">Stop Strapi</button>
-      <a href="http://52.47.167.109:1337/admin">Strapi Admin</a>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  function startStrapi() {
-    useFetch("https://vgqkqwfu6aseix5ejwdoqbxhi40rjxpi.lambda-url.eu-west-3.on.aws/").then(response => {
-      console.log("start" + response)
-    }).catch(response => {
-      console.log("stop" + response)
-    });
-  }
-
-  function stopStrapi() {
-    useFetch("https://5dtyb7orpjkgyf77wpxvhtcg4a0pytpa.lambda-url.eu-west-3.on.aws/").then(response => {
-      console.log("stop" + response)
-    }).catch(response => {
-      console.log("stop" + response)
-    });
-  }
-
 </script>
 
 <style scoped>
@@ -51,27 +30,5 @@ h2 {
   flex-direction: column;
   align-items: center;
   align-content: center;
-}
-
-.footer {
-  width: 20%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-evenly;
-
-  margin: 1rem;
-
-  font-family: 'San Francisco', monospace;
-  font-size: 1rem;
-
-  position: absolute;
-  bottom: 0;
-}
-
-.footer button {
-  background: #FCBF49;
-  border: black solid 0.15rem;
-  font-size: 1rem;
 }
 </style>
