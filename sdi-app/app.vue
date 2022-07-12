0dc3f065a881180a52667bef3210b7e054233a7e
Nathan Auguy <nathan.auguy@epita.fr>
2022-07-12 13:22:01 +0200

    Buttons for strapi

--- sdi-app/app.vue
+++ sdi-app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <NuxtLayout>
     <Home />
+    <div>
+      <button @click="startStrapi">Start Strapi</button>
+      <button @click="stopStrapi">Stop Strapi</button>
+      <a href="http://52.47.167.109:1337/admin">Strapi Admin</a>
+    </div>
   </NuxtLayout>
 </template>
 
@@ -18,6 +23,24 @@ export default {
   },
   components: { Home },
   layout: defaultVue
+}
+
+function startStrapi() {
+  useFetch("https://3fk05namec.execute-api.eu-west-3.amazonaws.com").then(response => {
+    console.log("start", response)
+    window.open("http://52.47.167.109:1337/admin", '_self')
+    console.log("opened")
+  }).catch(response => {
+    console.log("stop", response)
+  });
+}
+
+function stopStrapi() {
+  useFetch("https://3fk05namec.execute-api.eu-west-3.amazonaws.com/stop").then(response => {
+    console.log("stop", response)
+  }).catch(response => {
+    console.log("stop", response)
+  });
 }
 </script>
 
