967090221cabb7dd4ad83a8b21f92a3999b123b6
Nathan Auguy <nathan.auguy@epita.fr>
2022-07-12 14:13:24 +0200

    Strapi buttons

--- sdi-app/components/Home.vue
+++ sdi-app/components/Home.vue
@@ -4,10 +4,31 @@
     <h2>The place you go to fill you fish tanks, socks drawers and tractor garages</h2>
   </div>
   <ProductList/>
+  <div>
+      <button @click="startStrapi">Start Strapi</button>
+      <button @click="stopStrapi">Stop Strapi</button>
+      <a href="http://localhost:1337/admin">Strapi Admin</a>
+  </div>
 </template>
 
 <script setup lang="ts">
+  function startStrapi() {
+  useFetch("https://3fk05namec.execute-api.eu-west-3.amazonaws.com").then(response => {
+    console.log("start", response)
+    window.open("http://52.47.167.109:1337/admin", '_self')
+    console.log("opened")
+  }).catch(response => {
+    console.log("stop", response)
+  });
+}
 
+  function stopStrapi() {
+    useFetch("https://3fk05namec.execute-api.eu-west-3.amazonaws.com/stop").then(response => {
+      console.log("stop", response)
+    }).catch(response => {
+      console.log("stop", response)
+    });
+  }
 </script>
 
 <style scoped>
